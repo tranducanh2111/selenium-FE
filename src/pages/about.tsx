aae59cd087b3930e746b6ae97cfe7bdab3830e4d
Tran Duc Anh <tranducanh@sace.edu.vn>
2023-11-04 22:22:51 +1100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -25,17 +25,17 @@ const AboutUsPage: React.FC = () => {
             <img src={"/factory.jpg"} alt={"an image"} className='col-span-1 h-full w-full object-cover' />
           </div>
           <div className='max-w-[1160px] mx-auto px-5 sm:px-10'>
-            <p className='text-body'>Welcome to Selenium, your premier destination for sports gear, apparel, and equipment. We are passionate about sports and fitness, and we're here to help you pursue your athletic dreams. Our online store is dedicated to providing you with top-quality products and an unmatched shopping experience.</p>
+            <p className='text-body'>Welcome to Selenium, your premier destination for sports gear, apparel, and equipment. We are passionate about sports and fitness, and we&apos;re here to help you pursue your athletic dreams. Our online store is dedicated to providing you with top-quality products and an unmatched shopping experience.</p>
           </div>
           <div className='max-w-[1160px] mx-auto px-5 sm:px-10 space-y-[12px]'>
             <h2 className='text-h2 font-semibold'>Our Commitment to Quality</h2>
-            <p className='text-body'>We understand the importance of high-quality sports equipment and apparel in achieving your best performance. That's why we work tirelessly to curate a selection of the finest products from renowned brands, ensuring you have access to the best gear for your chosen sport.</p>
+            <p className='text-body'>We understand the importance of high-quality sports equipment and apparel in achieving your best performance. That&apos;s why we work tirelessly to curate a selection of the finest products from renowned brands, ensuring you have access to the best gear for your chosen sport.</p>
           </div>
           <div className="bg-white max-w-[1160px] flex flex-col mx-auto gap-[60px] px-5">
               <BlockSection />
               <div className='max-w-[1160px] mx-auto px-5 sm:px-10 space-y-[12px]'>
                 <h2 className='text-h2 font-semibold'>Stay up-to-date with us</h2>
-                <p className='text-body'>Stay updated with the latest sports trends, gear reviews, and exclusive offers by following us on social media. We love connecting with our customers and fellow athletes, so don't hesitate to reach out with any questions or feedback.</p>
+                <p className='text-body'>Stay updated with the latest sports trends, gear reviews, and exclusive offers by following us on social media. We love connecting with our customers and fellow athletes, so don&apos;t hesitate to reach out with any questions or feedback.</p>
               </div>
               <div className='max-w-[1160px] mx-auto px-5 sm:px-10 space-x-[24px]'>
                 <button className='hover:scale-[1.1] transition duration-200 ease-in-out'><a href=""><FacebookLogo /></a></button>
